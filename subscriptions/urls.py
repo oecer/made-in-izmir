e887a2587cb5787cdbd92be0c361378c5177fe90
Omer Ecer <eceromercan@gmail.com>
2026-03-21 23:12:20 +0300

    fix(urls): Correct subscription URL paths to match English terminology

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'subscriptions'
 
 urlpatterns = [
-    path('abonelik/', views.pricing_view, name='pricing'),
-    path('kullanici/abonelik/', views.my_subscription_view, name='my_subscription'),
+    path('subscription/', views.pricing_view, name='pricing'),
+    path('user/subscription/', views.my_subscription_view, name='my_subscription'),
 ]
